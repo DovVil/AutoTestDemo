f054a6b49abf7798b651b9a4b6ae20efbee7f858
Dovyduko kompiuteris <dovydas0417@gmail.com>
2022-10-06 15:00:49 +0300

    Add Thread.sleep into main/java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
         DayOfWeek weekDay = LocalDate.now().getDayOfWeek();
         System.out.println(weekDay);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
