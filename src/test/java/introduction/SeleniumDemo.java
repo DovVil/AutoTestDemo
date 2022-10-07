d80307a7f133517daf69926589547165afccfb17
Dovyduko kompiuteris <dovydas0417@gmail.com>
2022-10-07 14:11:24 +0300

    Implement annotations before and after method

--- src/test/java/introduction/SeleniumDemo.java
+++ src/test/java/introduction/SeleniumDemo.java
@@ -5,14 +5,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SeleniumDemo {
 
+    WebDriver driver;
+    @BeforeMethod
+    private void setUp(){
+        // pirmas metodas kuris vykdomas, keliam atsikartojancias setup'o eilutes
+    }
+
+    @AfterMethod
+    private void closeUp(){
+        // paskutinis metodas, irgi atsikartojancia info jei reik gale
+    }
+
+
     @Test
     public void testDemoWebDriver(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         driver.get("https://www.vilniuscoding.lt");    // atidaro psl.
 
@@ -31,7 +45,7 @@ public class SeleniumDemo {
     @Test
     public void testClickKontaktaiAtVCSpage(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://www.vilniuscoding.lt");
         driver.manage().window().maximize();
         WebElement kontaktaiVcs = driver.findElement(By.id("menu-item-3969"));
@@ -43,11 +57,31 @@ public class SeleniumDemo {
             e.printStackTrace();
         }
         driver.quit();
-
-
-
-
-
     }
 
+    @Test
+    public void testClickKontaktaiAtVCSpageWithSmallWindow() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.vilniuscoding.lt");
+        //driver.manage().window().maximize();
+
+//        WebElement buttonBurger = driver.findElement(By.id("burger"));
+//        buttonBurger.click();
+//
+//        WebElement kontaktaiVcs = driver.findElement(By.id("menu-item-3969"));
+//        kontaktaiVcs.click();
+        try {
+            WebElement buttonBurger = driver.findElement(By.id("burger"));
+            buttonBurger.click();
+            Thread.sleep(3500);
+
+            WebElement kontaktaiVcs = driver.findElement(By.id("menu-item-3969"));
+            kontaktaiVcs.click();
+            Thread.sleep(3500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
 }
